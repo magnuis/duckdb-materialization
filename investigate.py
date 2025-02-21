fc76da8f3d14d26afb7a47ff6c2e6092010ce78c
magnuis <sagmo.magnus@gmail.com>
2025-02-21 11:36:09 +0100

    Test tpch q2

--- investigate.py
+++ investigate.py
@@ -39,24 +39,6 @@ DF_COL_NAMES = [
 ]
 
 
-def _results_dfs(dataset: str, tests: list[str]):
-    old_results = dict()
-    current_run_no = 1
-    for test in tests:
-        try:
-            results = pd.read_csv(f"./testing/{dataset}/output/{test}.csv")
-            run_no = results['Test run no.'].max() + 1
-
-        except Exception:
-            results = pd.DataFrame(columns=DF_COL_NAMES)
-            run_no = 1
-
-        current_run_no = max(current_run_no, run_no)
-        old_results[test] = results
-
-    return old_results, current_run_no
-
-
 def _perform_test(
         con: duckdb.DuckDBPyConnection,
         dataset: str,
@@ -69,10 +51,10 @@ def _perform_test(
     results_df = pd.DataFrame(columns=DF_COL_NAMES)
     query_results = []  # List to store results from the first execution
 
-    for i, query_name in enumerate(queries, start=1):
+    for i, query in enumerate(queries, start=1):
 
-        with open(f"./queries/{dataset}/{query_name}.sql", 'r') as f:
-            query = f.read()
+        # with open(f"./queries/{dataset}/{query_name}.sql", 'r') as f:
+        #     query = f.read()
 
         df_row = {
             "Query": i,
@@ -102,6 +84,7 @@ def _perform_test(
             if j == 0:
 
                 first_run_result = result.copy()
+                print(f"Finished first test run in {execution_time} seconds")
 
         for j in range(iterations):
             df_row[f"malloc {j}"] = mallocs[j]
@@ -161,17 +144,9 @@ def perform_tests():
         if not os.path.isdir(f"./results/{dataset}"):
             os.mkdir(f"./results/{dataset}")
 
-        config = DATASETS[dataset]
 
-        queries: list = config["queries"]
-        tests_map: dict = config["tests_map"]
-        column_map: dict = config["column_map"]
 
         db_connection = duckdb.connect(f"./data/db/{dataset}.db")
-
-        # Load results dataframes and determine run number
-        old_result_dfs, run_no = _results_dfs(
-            dataset=dataset, tests=tests_map.keys())
 
         test_time = datetime.now()
 
@@ -182,26 +157,128 @@ def perform_tests():
         # Extract all fields to be used in these tests
         # all_fields = list(set([field for query in queries_map.values() for field in query]))
 
+        tests_map = {
+
+            "view":{
+                "queries": ["""
+SELECT
+    s.s_acctbal,
+    s.s_name,
+    n.n_name,
+    p.p_partkey,
+    p.p_mfgr,
+    s.s_address,
+    s.s_phone,
+    s.s_comment
+FROM
+    test_view p,
+    test_view s,
+    test_view ps,
+    test_view n,
+    test_view r
+WHERE
+    p.p_partkey = ps.ps_partkey
+    AND s.s_suppkey = ps.ps_suppkey
+    AND p.p_size = 15
+    AND p.p_type LIKE '%BRASS'
+    AND s.s_nationkey = n.n_nationkey
+    AND n.n_regionkey = r.r_regionkey
+    AND r.r_name = 'EUROPE'
+    AND ps.ps_supplycost = (
+        SELECT
+            MIN(ps.ps_supplycost)
+        FROM
+            test_view s,
+            test_view ps,
+            test_view n,
+            test_view r
+        WHERE
+            p.p_partkey = ps.ps_partkey
+            AND s.s_suppkey = ps.ps_suppkey
+            AND s.s_nationkey = n.n_nationkey
+            AND n.n_regionkey = r.r_regionkey
+            AND r.r_name = 'EUROPE'
+    )
+ORDER BY
+    s.s_acctbal DESC,
+    n.n_name,
+    s.s_name,
+    p.p_partkey
+LIMIT
+    100;
+
+"""], "column_map":tpch_setup.COLUMN_MAP
+            },
+                       "raw":{
+                "queries": ["""
+            SELECT
+            CAST(s.raw_json->>'s_acctbal' AS DECIMAL(12,2)) AS s_acctbal,
+            s.raw_json->>'s_name' AS s_name,
+            n.raw_json->>'n_name' AS n_name,
+            CAST(p.raw_json->>'p_partkey' AS INT) AS p_partkey,
+            p.raw_json->>'p_mfgr' AS p_mfgr,
+            s.raw_json->>'s_address' AS s_address,
+            s.raw_json->>'s_phone' AS s_phone,
+            s.raw_json->>'s_comment' AS s_comment
+     FROM
+            test_table p,
+            test_table s,
+            test_table ps,
+            test_table n,
+            test_table r
+     WHERE
+            CAST(p.raw_json->>'p_partkey' AS INT) = CAST(ps.raw_json->>'ps_partkey' AS INT)
+            AND CAST(s.raw_json->>'s_suppkey' AS INT) = CAST(ps.raw_json->>'ps_suppkey' AS INT)
+            AND CAST(p.raw_json->>'p_size' AS INT) = 15
+            AND (p.raw_json->>'p_type') like '%BRASS'
+            AND CAST(s.raw_json->>'s_nationkey' AS INT) = CAST(n.raw_json->>'n_nationkey' AS INT)
+            AND CAST(n.raw_json->>'n_regionkey' AS INT) = CAST(r.raw_json->>'r_regionkey' AS INT)
+            AND (r.raw_json->>'r_name') = 'EUROPE'
+            AND CAST(ps.raw_json->>'ps_supplycost' AS DECIMAL(12,2)) = (
+                    SELECT
+                            min(CAST(ps.raw_json->>'ps_supplycost' AS DECIMAL(12,2)))
+                    FROM
+                            test_table s,
+                            test_table ps,
+                            test_table n,
+                            test_table r
+                    WHERE
+                            CAST(p.raw_json->>'p_partkey' AS INT) = CAST(ps.raw_json->>'ps_partkey' AS INT)
+                            AND CAST(s.raw_json->>'s_suppkey' AS INT) = CAST(ps.raw_json->>'ps_suppkey' AS INT)
+                            AND CAST(s.raw_json->>'s_nationkey' AS INT)= CAST(n.raw_json->>'n_nationkey' AS INT)
+                            AND CAST(n.raw_json->>'n_regionkey' AS INT) = CAST(r.raw_json->>'r_regionkey' AS INT)
+                            AND (r.raw_json->>'r_name') = 'EUROPE'
+
+            )
+    ORDER BY
+            CAST(s.raw_json->>'s_acctbal' AS DECIMAL(12,2)) desc,
+            n.raw_json->>'n_name',
+            s.raw_json->>'s_name',
+            CAST(p.raw_json->>'p_partkey' AS INT)
+    LIMIT
+            100;
+"""],"column_map":{}
+            },
+        }
+
         for test, test_config in tests_map.items():
-            materialize_columns = test_config["materialization"]
-            if materialize_columns is None:
-                materialize_columns = column_map.keys()
 
             # Create the field-materialization setup for this test
             fields = []
-            for field, access_query in column_map.items():
+            for field, access_query in test_config["column_map"].items():
                 fields.append(
-                    (field, access_query, field in materialize_columns))
+                    (field, access_query, field in []))
 
             # Prepare database
-            prepare_database(con=db_connection, fields=fields)
+            if len(fields) > 0:
+                prepare_database(con=db_connection, fields=fields)
 
             # Run test
             new_results_df, query_result_df = _perform_test(
                 con=db_connection,
                 dataset=dataset,
-                queries=queries,
-                run_no=run_no,
+                queries=test_config["queries"],
+                run_no=1,
                 test_time=test_time
             )
 
@@ -212,8 +289,6 @@ def perform_tests():
                 f"./results/{dataset}/old_duckdb/{test}.csv", index=False)
 
             # Update df dicts
-            old_result_dfs[test] = pd.concat(
-                [old_result_dfs[test], new_results_df], ignore_index=True)
             new_results_dfs[test] = new_results_df
             query_results_dfs[test] = query_result_df
 
